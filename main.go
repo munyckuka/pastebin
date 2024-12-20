675fb8ce8b0e4381412b71f4366bdace6763cbb9
kuanysh <kuanyshkulbatsha@gmail.com>
2024-12-20 18:52:52 +0500

    Mongo connected. Pastes are creating and saving in db.

--- main.go
+++ main.go
@@ -11,7 +11,6 @@ func main() {
 	http.HandleFunc("/", server.MainPageHandler)
 	http.HandleFunc("/create-paste", server.CreatePasteHandler)
 	server.GetCollection("pastes")
-	server.ConnectToDB()
 	fmt.Println("Server is running on http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
 }
